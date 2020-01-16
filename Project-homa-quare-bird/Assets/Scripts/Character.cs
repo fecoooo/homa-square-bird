4c941181b33e6d6af57242e93f787c615784e7bc
Unknown <dajka1989@gmail.com>
2020-01-16 18:04:03 +0100

    !WIP camera, character basic shit

---
+++ Project-homa-quare-bird/Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	new Rigidbody rigidbody;
+
+    void Start()
+    {
+		rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+
+		}
+    }
+
+
+	public void SpawnEgg()
+	{
+		GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	}
+}
