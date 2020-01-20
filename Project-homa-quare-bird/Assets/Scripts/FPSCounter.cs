6aa8d56adb557de091c608facc0454cfd59489df
Unknown <dajka1989@gmail.com>
2020-01-20 13:25:38 +0100

    !A FPS Counter

---
+++ Project-homa-quare-bird/Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FPSCounter:MonoBehaviour
+{
+	TextMeshProUGUI fpsCounterText;
+	int prevFPS;
+
+	private void Start()
+	{
+		fpsCounterText = GetComponent<TextMeshProUGUI>();
+	}
+
+	private void Update()
+	{
+		int avgFrameRate = Mathf.CeilToInt(1f / Time.unscaledDeltaTime);
+		if (prevFPS == avgFrameRate)
+			fpsCounterText.text = avgFrameRate.ToString() + " FPS";
+		prevFPS = avgFrameRate;
+	}
+}
