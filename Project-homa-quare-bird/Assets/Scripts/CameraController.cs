2b7215efd4fce7807f60850a3ab9bde6f91c2abf
Unknown <dajka1989@gmail.com>
2020-01-21 14:41:32 +0100

    !A pit to test map, !U camera is not attached to the character prefab anymore

---
+++ Project-homa-quare-bird/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform focus;
+	float differenceOnStart;
+
+	private void Start()
+	{
+		differenceOnStart = transform.position.x - focus.position.x;
+	}
+
+	void Update()
+    {
+		transform.position = new Vector3(focus.position.x + differenceOnStart, transform.position.y, transform.position.z);
+    }
+}
