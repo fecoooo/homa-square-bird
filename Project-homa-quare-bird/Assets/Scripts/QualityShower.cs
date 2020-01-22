e32edff996850f2d579685ee4049e1120dd3a771
Unknown <dajka1989@gmail.com>
2020-01-22 17:05:55 +0100

    !F QuailityShower

---
+++ Project-homa-quare-bird/Assets/Scripts/QualityShower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QualityShower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		GetComponent<TextMeshProUGUI>().text = QualitySettings.GetQualityLevel().ToString();
+
+	}
+}
