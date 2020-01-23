6b5d79695e6cea5a391ea96f53d09f56471b80b6
Unknown <dajka1989@gmail.com>
2020-01-23 14:30:38 +0100

    !F Change "gamma" with slider

---
+++ Project-homa-quare-bird/Assets/Scripts/GammaChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GammaChanger : MonoBehaviour
+{
+	Slider slider;
+
+	private void Start()
+	{
+		slider = GetComponent<Slider>();
+	}
+
+	public void OnGammaChange()
+	{
+		RenderSettings.ambientLight = new Color(slider.value, slider.value, slider.value, 1.0f);
+	}
+}
