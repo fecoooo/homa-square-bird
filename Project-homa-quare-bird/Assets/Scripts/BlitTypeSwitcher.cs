1b9bd6cbc8587e40617008ae9a4276681ad639a9
Unknown <dajka1989@gmail.com>
2020-01-24 10:40:20 +0100

    !U Set things for Android build

---
+++ Project-homa-quare-bird/Assets/Scripts/BlitTypeSwitcher.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BlitTypeSwitcher : MonoBehaviour
+{
+	private void Start()
+	{
+		if(Application.isPlaying)
+			Destroy(gameObject);
+	}
+
+	void Update()
+    {
+		if(PlayerSettings.Android.blitType != AndroidBlitType.Always)
+			PlayerSettings.Android.blitType = AndroidBlitType.Always;
+	}
+}
+#endif
